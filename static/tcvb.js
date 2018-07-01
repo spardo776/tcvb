f98caffdd4be2afd695538273cd9ddd2ba2d62d9
sebastien.pardo <sebastien.pardo@cegetel.net>
2018-07-01 17:23:32 +0100

    -

---
+++ static/tcvb.js
@@ -0,0 +1,53 @@
+"use strict";
+
+
+var app = new Vue({
+    el: '#go_vue_app',
+    data: {
+        filter: '',
+        groups: [],
+        isfree: true,
+        noresult: false
+    },
+    methods: {
+        f_filter: function () {
+            var lo_app_data = this;
+            console.log("filter=" + lo_app_data.filter);
+            var ls_url = "http://localhost:8080/api/group?";
+            var lb_filtered = false;
+
+            if ((!lb_filtered) && lo_app_data.filter.match('\d+')) {
+                ls_url = ls_url + "year=" + lo_app_data.filter;
+                lb_filtered = true;
+            }
+            if ((!lb_filtered) && (lo_app_data.filter.match('lundi|mardi|mercredi|jeudi|vendredi|samedi|dimanche'))) {
+                ls_url = ls_url + "day=" + lo_app_data.filter;
+                lb_filtered = true;
+            }
+            if ((!lb_filtered) && (lo_app_data.filter.match('vert|orange|violet'))) {
+                ls_url = ls_url + "level=" + lo_app_data.filter;
+                lb_filtered = true;
+            }
+            if (lo_app_data.isfree) {
+                ls_url = ls_url + (lb_filtered ? "&is_free" : "is_free");
+            }
+            console.log("url=" + ls_url);
+
+
+            axios.get(ls_url).then(
+                function (response) {
+                    console.log("found=" + response.data.length);
+                    lo_app_data.noresult = (response.data.length === 0);
+                    lo_app_data.groups = response.data;
+
+                }); //TODO error handling
+
+
+        },
+        f_open_group : function(){
+            console.log('open group=')
+        }
+
+    }
+   
+})
